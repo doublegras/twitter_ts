afc234825bd84fd3e02cd6f4bcff54b465a4345b
Marc <marc@ambuhl.ch>
2021-04-09 21:11:09 +0200

    first commit

--- env/dev.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  dbUrl: "mongodb+srv://marc:Dfrvnzazoo22_Mb@cluster0.rhhgb.mongodb.net/twitter?retryWrites=true&w=majority",
-  cert: path.join(__dirname, '../ssl/local.crt'),
-  key: path.join(__dirname, '../ssl/local.key')
-}
--- env/prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  dbUrl: "mongodb+srv://marc:Dfrvnzazoo22_Mb@cluster0.rhhgb.mongodb.net/twitter?retryWrites=true&w=majority",
-}
